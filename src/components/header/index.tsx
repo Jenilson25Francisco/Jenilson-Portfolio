a8ce8175cc45f05d0115395b14e9436396db996f
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-03-26 18:03:48 +0100

    feat: add Header component and utility function for class name merging

---
+++ src/components/header/index.tsx
@@ -0,0 +1,28 @@
+import { HomeIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className="absolute top-0 w-full z-10 h-24 flex items-center justify-center">
+      <div className="flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            width={84}
+            height={32}
+            alt="Jenilson FRancisco"
+          />
+        </Link>
+
+        <nav className="flex items-center gap-4 sm:gap-2">
+          <Link href="/">
+            <HomeIcon />
+            <span>Home</span>
+          </Link>
+          <Link href="/"></Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
