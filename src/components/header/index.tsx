28f5743feaa458970ccd0b4e2bc6623c9b1c3176
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-04-20 17:34:02 +0100

    fix: removing p tag

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,19 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
 import { NavItems } from "../NavItems";
-import Image from "next/image";
 
 export function Header() {
   return (
-    <header className="w-full z-10 h-24 flex items-center justify-between px-5 md:max-w-[1200px] mx-auto">
-      <Link href="/#">
-        <p className="font-bold text-4xl">
-          <Image
-            src="/JenilsonFrancisco.png"
-            alt="Jenilson Francisco"
-            width={175}
-            height={44}
-          />
-        </p>
+    <header className="w-full z-10 h-24 shadow-xl flex items-center justify-between px-5 md:max-w-[1200px] mx-auto">
+      <Link href="/">
+        <Image
+          src="/JenilsonFrancisco.png"
+          alt="JenilsonFrancisco"
+          width={175}
+          height={44}
+        />
       </Link>
       <NavItems />
     </header>
